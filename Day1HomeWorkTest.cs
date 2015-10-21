82fff7b8ef382c86dc13ff4059b6fca64180e328
matt <matt.yeh@allpay.com.tw>
2015-10-21 15:48:31 +0800

    Day 1 homework

---
+++ Day1HomeWorkTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExpectedObjects;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace InternalLibTests
+{
+    [TestClass()]
+    class Day1HomeWorkTest
+    {
+        [TestMethod()]
+        public void getCostSumBy3()
+        {
+           var homeWorkPaging = new HomeWorkPaging();
+           int groupNumber = 3;
+
+           var target = homeWorkPaging.getCostSumArray(groupNumber);
+
+           var excepted = new int[] { 6, 15, 24, 24 };
+
+           excepted.ToExpectedObject().ShouldEqual(target);
+        }
+
+        [TestMethod()]
+        public void getRevenueSumBy4()
+        {
+            var homeWorkPaging = new HomeWorkPaging();
+            int groupNumber = 4;
+
+            var target = homeWorkPaging.getRevenueArray(groupNumber);
+
+            var excepted = new int[] { 50, 66, 60 };
+
+            excepted.ToExpectedObject().ShouldEqual(target);
+        }
+    }
+}
